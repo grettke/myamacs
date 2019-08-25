97906e451aafca3729efd69a0780f5e7cda5655c
Grant Rettke <gcr@wisdomandwonder.com>
2019-08-24 19:16:29 -0500

    Add Org version check

--- .emacs.el
+++ .emacs.el
@@ -11,11 +11,21 @@
 ;;; Org mode
 
 (load-file "~/src/myamacs/org-mode-ecm.el")
+(let ((release "9.1.9"))
+  (when (not (version= release org-version))
+    (display-warning
+     'myamacs
+     (format
+      (concat "Sorry, I might have problems running right now. It looks like "
+              "version %s of Org mode is installed but I require "
+              "version %s of Org mode to run predictably.")
+      org-version release)
+     :error)))
 (load-file "~/src/myamacs/org-mode-literate-fundamental-configuration.el")
 ;; Required by Org mode
 (add-to-list 'load-path "~/src/htmlize")
 (require 'htmlize)
-;; Required for ox-koma-letter. Checkout same tag as Emacs's version.
+;; Required for ox-koma-letter. Use a branch of the release tag.
 (add-to-list 'load-path "~/src/org-mode/contrib/lisp")
 (load-file "~/src/myamacs/org-mode.el")
 (load-file "~/src/myamacs/org2blog-config.el")
