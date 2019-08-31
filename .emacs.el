e4a70f8ba453fc489b60ff0b83b2f4e7860c6350
Grant Rettke <grant@wisdomandwonder.com>
2019-08-31 15:57:19 -0500

    MacCadet

--- .emacs.el
+++ .emacs.el
@@ -39,12 +39,7 @@
 
 ;;;; macOS
 
-(setq ns-control-modifier 'control)
-(setq ns-command-modifier 'meta)
-(setq ns-alternate-modifier 'hyper)
-(setq ns-right-control-modifier 'alt)
-(setq ns-right-command-modifier 'super)
-(setq ns-right-alternate-modifier 'none)
+(load-file "~/src/maccadet/maccadet.el")
 
 ;;;; Utility Functions
 
