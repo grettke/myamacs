ca942d9b43d5675b122f85348a7d0eddf83f8d57
Grant Rettke <grant@wisdomandwonder.com>
2019-10-17 22:32:16 -0500

    Keybindings

---
+++ keybindings.el
@@ -0,0 +1,74 @@
+;; -*- lexical-binding: t; no-byte-compile: t; coding: utf-8 -*-
+
+;;;; Key Bindings
+
+;;; Row 4: 1...
+
+(global-set-key (kbd "A-9") #'org2blog-user-interface)
+(global-set-key (kbd "A-0") #'org2blog-complete)
+(global-set-key (kbd "A--") #'gcr-insert-em-dash)
+(global-set-key (kbd "C-A--") #'gcr-insert-en-dash)
+(global-set-key (kbd "A-=") #'gcr-open-in-textedit)
+
+;;; Row 3: Q...
+
+(global-set-key (kbd "A-q") #'fill-paragraph)
+(global-set-key (kbd "C-A-q") #'gcr-unfill-paragraph)
+
+(global-set-key (kbd "A-w") #'kill-ring-save)
+
+(global-set-key (kbd "A-e") #'kmacro-end-or-call-macro)
+
+(global-set-key (kbd "A-r") #'gcr-replace-string)
+
+(global-set-key (kbd "A-t") #'gcr-ui-date)
+
+(global-set-key (kbd "A-p") (lambda () (interactive) (other-window -1)))
+
+(global-set-key (kbd "A-o") #'find-file)
+
+(global-set-key (kbd "A-\\") #'gcr-indent-buffer)
+
+;;; Row 2: A...
+
+(global-set-key (kbd "A-a") #'gcr-beginning-of-line-dwim)
+
+(global-set-key (kbd "A-d") #'kill-word)
+
+(global-set-key (kbd "A-s") #'shell)
+
+(global-set-key (kbd "A-f") #'forward-word)
+
+(global-set-key (kbd "A-g") #'gcr-occur-dwim)
+
+(global-set-key (kbd "A-j") #'switch-to-buffer)
+
+(global-set-key (kbd "A-k") #'execute-extended-command)
+
+(global-set-key (kbd "A-l") #'gcr-switch-to-previous-buffer)
+
+(global-set-key (kbd "A-;") #'eval-expression)
+
+;;; Row 1: Z...
+
+(global-set-key (kbd "A-z") (kbd "C-c C-k"))
+
+(global-set-key (kbd "A-b") 'Control-X-prefix)
+
+(global-set-key (kbd "A-c") (kbd "C-c C-c"))
+
+(global-set-key (kbd "A-b") #'backward-word)
+
+(global-set-key (kbd "A-n") #'other-window)
+
+(global-set-key (kbd "A-<") #'beginning-of-buffer)
+
+(global-set-key (kbd "A->") #'end-of-buffer)
+
+(global-set-key (kbd "A-/") #'winner-undo)
+
+(global-set-key (kbd "C-A-/") #'winner-redo)
+
+;;; Row 0: Ctrl...
+
+(global-set-key (kbd "A-SPC") #'set-mark-command)
