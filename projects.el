0248794c5055da01983bab71b9f04a11ddf42e81
Grant Rettke <grant@wisdomandonder.com>
2019-10-28 16:00:07 -0500

    Don't load projects by default

--- projects.el
+++ projects.el
@@ -1,17 +1,18 @@
 ;; -*- lexical-binding: t; no-byte-compile: t; coding: utf-8 -*-
 
-(add-to-list 'load-path "~/src/pretty-mode")
-(require 'pretty-mode)
+(when nil
+  (add-to-list 'load-path "~/src/pretty-mode")
+  (require 'pretty-mode)
 
-(add-to-list 'load-path "~/src/universal-emotions-emoticons")
-(require 'universal-emotions-emoticons)
+  (add-to-list 'load-path "~/src/universal-emotions-emoticons")
+  (require 'universal-emotions-emoticons)
 
-(add-to-list 'load-path "~/src/corporate-bs-generator")
-(require 'corporate-bs-generator)
+  (add-to-list 'load-path "~/src/corporate-bs-generator")
+  (require 'corporate-bs-generator)
 
-(add-to-list 'load-path "~/src/org-utf-to-xetex")
-(require 'org-utf-to-xetex)
-(add-hook 'org-mode-hook #'org-utf-to-xetex-prettify)
+  (add-to-list 'load-path "~/src/org-utf-to-xetex")
+  (require 'org-utf-to-xetex)
+  (add-hook 'org-mode-hook #'org-utf-to-xetex-prettify)
 
-(add-to-list 'load-path "~/src/say")
-(require 'say)
+  (add-to-list 'load-path "~/src/say")
+  (require 'say))
